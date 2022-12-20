d6acd5c92a4bb7ee281131e88525fdca416dc4c4
Patrick <pat.trainer@gmail.com>
2022-12-19 22:34:15 -0600

    feat: build outages db

---
+++ entergy_outages/build_db.py
@@ -0,0 +1,12 @@
+import utils
+
+
+if __name__ == "__main__":
+
+    ref = "main"
+    filepath = "entergy_outages_zipcode.json"
+    file_versions = utils.iterate_file_versions(".", filepath, ref=ref)
+    db = utils.sqlite_utils.Database("entergy_outages.db", recreate=True)
+
+    utils.insert_from_file(file_versions, db)
+
--- entergy_outages/utils.py
+++ entergy_outages/utils.py
@@ -1,4 +1,11 @@
 from datetime import datetime
+from pathlib import Path
+import hashlib
+import json
+
+import git
+import sqlite_utils
+
 
 def from_epoch(epoch_time: int) -> str:
     """
@@ -8,6 +15,7 @@ def from_epoch(epoch_time: int) -> str:
     epoch_time = int(str(epoch_time)[:-3])
     formatted = datetime.fromtimestamp(epoch_time).isoformat(timespec="auto")
     return formatted
+
 
 def today() -> str:
     """
@@ -22,13 +30,32 @@ def iterate_file_versions(repo_path, filepath, ref="main"):
     """
     repo = git.Repo(repo_path, odbt=git.GitCmdObjectDB)
     commits = reversed(list(repo.iter_commits(ref, paths=filepath)))
-    
+
     for commit in commits:
         commit_tree = commit.tree
         blobs = commit_tree.blobs
 
         try:
-            blob = [b for b in blobs if b.name == Path(filepath).name][0] 
+            blob = [b for b in blobs if b.name == Path(filepath).name][0]
             yield commit.committed_datetime, commit.hexsha, blob.data_stream.read()
-        except git.GitCommandError:
-            pass
+        except Exception:
+            continue
+
+
+def insert_from_file(file_versions, db) -> None:
+    for i, (commit_time, commit_hash, file_contents) in enumerate(file_versions):
+        try:
+            outages = json.loads(file_contents)
+        except ValueError:
+            continue
+
+        for outage in outages:
+            # create a unique id for each outage
+            outage_id = outage["zip"] + outage["_loaded_at"]
+            outage_id = hashlib.md5(outage_id.encode()).hexdigest()
+            db["outages"].insert(
+                dict(outage, id=outage_id),
+                pk="id",
+                alter=True,
+                replace=True,
+            )
